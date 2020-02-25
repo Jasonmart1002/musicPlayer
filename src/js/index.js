a45d5abe98518ad633c18c3ecc7ab4c041f974d0
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-02-25 16:54:59 -0500

    main player doesnt work

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
@@ -8,8 +8,69 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+const Songs = () => {
+	const [music, setMusic] = useState(null);
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+	const [source, setSource] = useState(
+		"https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3"
+	);
+
+	useEffect(
+		() =>
+			fetch("https://assets.breatheco.de/apis/sound/songs")
+				.then(r => r.json())
+				.then(data => setMusic(data)),
+		[]
+	);
+
+	return (
+		<div className="container text-center">
+			<ul>
+				{music === null
+					? "Loading..."
+					: music.map((t, index) => (
+							<li
+								key={index}
+								id="mysound"
+								className="songs"
+								onClick={() => {
+									setSource(
+										"https://assets.breatheco.de/apis/sound/" +
+											t.url
+									);
+									console.log(source);
+								}}>
+								{t.name}
+								<audio controls>
+									<source
+										src={
+											"https://assets.breatheco.de/apis/sound/" +
+											t.url
+										}
+										type="audio/mpeg"
+									/>
+									Your browser does not support the audio
+									element.
+								</audio>
+							</li>
+					  ))}
+			</ul>
+			<audio controls>
+				<source src={source} type="audio/mpeg" />
+				Your browser does not support the audio element.
+			</audio>
+		</div>
+	);
+};
+
+ReactDOM.render(<Songs />, document.querySelector("#app"));
+
+{
+	/* <audio>
+	<source
+		src={"https://assets.breatheco.de/apis/sound/" + t.url}
+		type="audio/mpeg"
+	/>
+	Your browser does not support the audio element.
+</audio>; */
+}
